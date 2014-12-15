54df5f7a1418d9bbf389a010e0006df39f1e64e8
Colin Brevitz <verlander94@gmail.com>
2014-12-15 15:12:29 -0500

    Initial commit

---
+++ MyMazeTest.java
@@ -0,0 +1,35 @@
+import org.junit.Test;
+
+
+public class MyMazeTest {
+
+	@Test
+	public void testGenerateMaze() {
+
+	}
+
+	@Test
+	public void testSolveMaze() {
+
+	}
+
+	@Test
+	public void testToGraph() {
+
+	}
+	
+	@Test
+	public void testToArray() {
+
+	}
+
+	@Test
+	public void testStartVertex() {
+
+	}
+
+	@Test
+	public void finishVertex() {
+
+	}
+}
---
+++ MyVertex.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class MyVertex implements Vertex {
+
+	@Override
+	public Pair getElement() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setElement(Pair e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Edge> incidentEdges() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ArrayList<Vertex> adjacentVertices() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
